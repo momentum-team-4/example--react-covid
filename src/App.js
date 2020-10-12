2c2dec1f903b8c3a7d41ee3abb37f769324b7df4
Clinton Dreisbach <clinton@dreisbach.us>
2020-10-12 11:34:57 -0400

    Get list of countries

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* globals fetch */
 
-function App() {
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
+function App () {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.covid19api.com/countries')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        data.sort((a, b) => {
+          if (a.Country < b.Country) {
+            return -1
+          } else if (a.Country > b.Country) {
+            return 1
+          } else {
+            return 0
+          }
+        })
+        setCountries(data)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ul>
+        {countries.map(country => (
+          <li key={country.ISO2}>{country.Country}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
